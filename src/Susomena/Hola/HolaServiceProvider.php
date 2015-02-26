dd91b0e271a82baf4b72cee923f59f774c5b98d4
Jesus Mena <jmena@eglesys.com>
2015-02-26 13:52:10 +0100

    Added basic package structure

---
+++ src/Susomena/Hola/HolaServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Susomena\Hola;
+
+use Illuminate\Support\ServiceProvider;
+
+class HolaServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('susomena/hola');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
